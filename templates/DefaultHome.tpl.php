21262929585aac6f24490d761562d274411d87de
webadmin <webadmin@erparker.(none)>
2013-12-13 01:01:17 +0400

    Pushing up to repo

--- templates/DefaultHome.tpl.php
+++ templates/DefaultHome.tpl.php
@@ -11,7 +11,7 @@
       <div class="modal-content">
         <div class="modal-header">
           <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-          <h4 class="modal-title">Cars with Best MPG</h4>
+          <h4 class="modal-title">Cars just like yours</h4>
         </div>
         <div class="modal-body">
           <table id="maintbl" class="table table-bordered table-striped table-hover">
